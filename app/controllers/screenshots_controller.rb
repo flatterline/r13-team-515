37e4b8e2ae118211435ce1c1bffd5b3ee6c88b7f
Jim Jeffers <jim@sumocreations.com>
2013-10-18 19:51:32 -0700

    Added default action for screenshots controller.

--- app/controllers/screenshots_controller.rb
+++ app/controllers/screenshots_controller.rb
@@ -3,6 +3,16 @@ class ScreenshotsController < ApplicationController
   ##
   # Provides the latest screenshots available from
   # two news sources.
-  def comparison; end
+  def index
+
+    # Pseudocode --
+    # Left source should be a random source as well as the
+    # right source. The right source should never be assigned
+    # to the same source as the left source.
+    #
+    # @left_source = Source.random
+    # @right_source = Source.random.except(@left_source)
+
+  end
 
 end
